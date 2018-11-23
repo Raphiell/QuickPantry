5d38f4ea5c16034b953a9e217aef9906aa32d7aa
Raphiell <rs_cone@live.com>
2018-11-23 11:33:00 -0600

    Created Item Adapter class

---
+++ app/src/main/java/com/example/quickpantry/ListView/ItemAdapter.java
@@ -0,0 +1,8 @@
+package com.example.quickpantry.ListView;
+
+import android.widget.ArrayAdapter;
+
+import com.example.quickpantry.Database.Item;
+
+public class ItemAdapter extends ArrayAdapter<Item> {
+}
